1e5e15f33bcaf0db9033fbf45014c1d194e3adce
CakieJoy <cakiejoy@utkan.net>
2026-05-30 23:11:50 +0300

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+#!/bin/sh
+
+# Geçici klasördeki güncel kaynak kodları,
+# dışarıdan bağlanan /app klasörünün içine kopyala (mevcut config ve uploadlara dokunmaz)
+echo "Source code copying on volume"
+cp -r /app_tmp/. /app/
+
+# Sunucuyu başlat
+echo "Starting API"
+exec uvicorn main:app \
+  --host 0.0.0.0 \
+  --port 8000 \
+  --workers 4 \
+  --loop uvloop \
+  --http httptools \
+  --no-access-log \
+  --limit-concurrency 1000 \
+  --timeout-keep-alive 10
---
+++ main.py
@@ -0,0 +1,80 @@
+from logging import config
+
+from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, UploadFile, File, Depends, HTTPException, Security
+from fastapi.security.api_key import APIKeyHeader
+import os
+import uuid
+import yaml
+app = FastAPI()
+API_KEY_NAME = "X-API-KEY"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
+
+def API_key_check(entry_key: str = Security(api_key_header)):
+    if entry_key != API_KEY:
+        raise HTTPException(status_code=403, detail="API Key is invalid")
+    return {
+        "status": "403",
+        "message": "API Key is valid"
+    }
+
+@app.post("/reload_config")
+def reload_config(security: str = Depends(API_key_check)):
+    global UPLOAD_FOLDER, DOMAIN, API_KEY, IMAGE_URL_PREFIX, SUPPORTED_EXTENSIONS
+    with open("config.yaml", "r") as config_file:
+        config = yaml.safe_load(config_file)
+
+    UPLOAD_FOLDER = config["settings"]["UPLOAD_FOLDER"]  # * Folder name for uploaded images.
+    DOMAIN = config["settings"]["DOMAIN"]  # * your domain name or ip address (yeah ip addrsses work too but name is domain :3)
+    API_KEY = config["settings"]["API_KEY"]  # * CHANGE THIS TO STRONG API KEY!!111!1!1!!11!1!1
+    IMAGE_URL_PREFIX = config["settings"]["URL_PREFIX"]  # ! not working on reload still need restart the APı
+    SUPPORTED_EXTENSIONS = config["supported_extensions"]  # * supported image extensions
+    return {"status": "200", "message": "Configuration reloaded successfully"}
+# 1. Bekçinin hangi başlığa (Header) bakacağını seçiyoruz
+
+
+reload_config()  # * Load configuration at startup
+
+
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+# * ^ this code checks if the folder exists and if doesnt create the folder
+# 3. Şifreyi kontrol eden kontrol fonksiyonu
+
+
+app.mount(IMAGE_URL_PREFIX, StaticFiles(directory=UPLOAD_FOLDER), name="gorseller")
+
+@app.post("/upload")
+def upload_image(image: UploadFile = File(...), security: str = Depends(API_key_check)):
+    image_extension = os.path.splitext(image.filename)[1]
+    
+    if image_extension in SUPPORTED_EXTENSIONS:
+
+        image_id = f"{uuid.uuid4()}{image_extension}"
+
+        image_path = os.path.join(UPLOAD_FOLDER, image_id)
+
+        with open(image_path, "wb") as buffer:
+            buffer.write(image.file.read())
+
+        access_url = f"http://{DOMAIN}{IMAGE_URL_PREFIX}/{image_id}"
+        return {
+            "status": "200",
+            "image_id": image_id,
+            "url": access_url
+        }
+    else:
+        raise HTTPException(status_code=400, detail={"message": "Unsupported file type", "supported_extensions": SUPPORTED_EXTENSIONS})
+
+
+@app.post("/delete")
+def delete_image(image_id: str, security: str = Depends(API_key_check)):
+    image_path = os.path.join(UPLOAD_FOLDER, image_id)
+    if os.path.exists(image_path):
+        os.remove(image_path)
+        return {"status": "200", "message": "Image deleted successfully"}
+    else:
+        raise HTTPException(status_code=404, detail="Image not found")

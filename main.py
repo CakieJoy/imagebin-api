47aede0d2da0416c13cac0732e745db7c9a0acb8
CakieJoy <cakiejoy@utkan.net>
2026-06-04 23:30:15 +0300

    feat: Added 404 HTML page

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from logging import config
+
+
 from auth import API_key_check
 from delete import delete_image
 from fastapi import FastAPI
@@ -6,13 +8,15 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI, UploadFile, File, Depends, HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 import os
 import app_config as config
 from upload import upload_image
 from delete import delete_image
 
 fastapi_kwargs = {}
-
+templates = Jinja2Templates(directory="/app/data/templates")
 
 if config.DISABLE_DOCS:  
     fastapi_kwargs.update({"docs_url": None, "redoc_url": None})
@@ -26,9 +30,6 @@ app = FastAPI(**fastapi_kwargs)
 def reload_config_endpoint(security: str = Depends(API_key_check)):
     config.reload_config()
     return  {"status": "200", "message": "Configuration reloaded successfully"}
-
-
-
 
 if not os.path.exists("/app/data/" + config.UPLOAD_FOLDER):
     os.makedirs("/app/data/" + config.UPLOAD_FOLDER)
@@ -45,3 +46,13 @@ def upload(image: UploadFile = File(...), security: str = Depends(API_key_check)
 @app.delete("/v1/delete")
 def delete(image_id: str, security: str = Depends(API_key_check)):
     return delete_image(image_id, security)
+
+
+@app.exception_handler(404)
+async def not_found(request, exc: Exception):
+    return templates.TemplateResponse(
+     request=request,
+     name="404.html",
+     context={"request": request},
+     status_code=404
+    )

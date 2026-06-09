ac2e5309111f5b953f71b388b2ecdf9e07ce86f3
CakieJoy <cakiejoy@utkan.net>
2026-06-10 01:13:32 +0300

    fix: API key plain text issue. now API key is hashed

--- app_config.py
+++ app_config.py
@@ -3,20 +3,20 @@ import yaml
 
 UPLOAD_FOLDER = "images"
 DOMAIN = "localhost:8000"
-API_KEY = "my_very_very_secret_api_key"
+RAW_API_KEY = "my_very_very_secret_api_key"
 IMAGE_URL_PREFIX = "/images"
 SUPPORTED_EXTENSIONS = [".jpg", ".jpeg", ".png", ".gif"]
 DISABLE_DOCS = True
 
 def reload_config():
-    global UPLOAD_FOLDER, DOMAIN, API_KEY, IMAGE_URL_PREFIX, SUPPORTED_EXTENSIONS, DISABLE_DOCS
+    global UPLOAD_FOLDER, DOMAIN, RAW_API_KEY, IMAGE_URL_PREFIX, SUPPORTED_EXTENSIONS, DISABLE_DOCS
     
     with open("/app/data/config.yaml", "r") as config_file:
         data = yaml.safe_load(config_file)
 
     UPLOAD_FOLDER = data["settings"]["UPLOAD_FOLDER"]
     DOMAIN = data["settings"]["DOMAIN"]
-    API_KEY = data["settings"]["API_KEY"]
+    RAW_API_KEY = data["settings"]["API_KEY"]
     IMAGE_URL_PREFIX = data["settings"]["URL_PREFIX"]
     SUPPORTED_EXTENSIONS = data["supported_extensions"]
     DISABLE_DOCS = data["settings"].get("DISABLE_DOCS")
--- auth.py
+++ auth.py
@@ -1,22 +1,26 @@
-import os
-import uuid
-from fastapi import FastAPI, UploadFile, File, Depends, HTTPException, Security
+import hashlib
+import secrets
+from fastapi import HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
-from fastapi.staticfiles import StaticFiles
-import yaml
 import app_config
 
 
-API_KEY = app_config.API_KEY  
+RAW_API_KEY = app_config.RAW_API_KEY
+HASHED_API_KEY = hashlib.sha256(RAW_API_KEY.encode()).hexdigest()
+
+del RAW_API_KEY
+del app_config.RAW_API_KEY
 
 
 API_KEY_NAME = "X-API-KEY"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
 
 def API_key_check(entry_key: str = Security(api_key_header)):
-    if entry_key != API_KEY:
+    HASHED_ENTRY_KEY = hashlib.sha256(entry_key.encode()).hexdigest()
+    del entry_key
+    if not secrets.compare_digest(HASHED_ENTRY_KEY, HASHED_API_KEY):
         raise HTTPException(status_code=403, detail="API Key is invalid")
     return {
-        "status": "403",
+        "status": "200",
         "message": "API Key is valid"
     }

12616bd052ff794de48584864557beee00e0e57b
CakieJoy <cakiejoy@utkan.net>
2026-07-15 22:22:29 +0300

    added try catch and if not existing key trying to delete returns 404 (v2 del key)

--- auth/v2_del_key.py
+++ auth/v2_del_key.py
@@ -7,17 +7,22 @@ from fastapi.security import APIKeyHeader
 API_KEY_NAME = "X-API-KEY"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
 
-# todo: add try-catch and try delete non existing key
 def Delete_API_key_AuthV2(entry_key: str = Security(api_key_header), req_permission: str = "a"):
-    conn = sqlite3.connect('/app/data/api_keys.db')
-    cursor = conn.cursor()
-    hashed_entry_key = hashlib.sha256(entry_key.encode()).hexdigest()
-    cursor.execute("DELETE FROM api_keys WHERE api_key = ?", (hashed_entry_key,))
-    conn.commit()
-    conn.close()
-    del entry_key
-    del hashed_entry_key
-    return {
-        "status": "200",
-        "message": "API Key deleted successfully"
-    }
+    try:
+        conn = sqlite3.connect('/app/data/api_keys.db')
+        cursor = conn.cursor()
+        hashed_entry_key = hashlib.sha256(entry_key.encode()).hexdigest()
+        cursor.execute("DELETE FROM api_keys WHERE api_key = ?", (hashed_entry_key,))
+        if cursor.rowcount == 0:
+            raise HTTPException(status_code=404, detail="API Key not found")
+        else:
+            conn.commit()
+            conn.close()
+            del entry_key
+            del hashed_entry_key
+            return {
+                "status": "200",
+                "message": "API Key deleted successfully"
+            }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Error occurred while deleting API key")

9a4c1b2c4ac4355bad9f8b5c20e3231ba08e0761
CakieJoy <cakiejoy@utkan.net>
2026-07-18 16:34:53 +0300

    fix: del_key works with the new db system

--- auth/v2_del_key.py
+++ auth/v2_del_key.py
@@ -7,22 +7,24 @@ from fastapi.security import APIKeyHeader
 API_KEY_NAME = "X-API-KEY"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
 
-def Delete_API_key_AuthV2(entry_key: str = Security(api_key_header), req_permission: str = "a"):
+def Delete_API_key_AuthV2(entry_key: str, req_permission: str = "a", security: str = Security(api_key_header)):
     try:
+        uid_part, key_part = entry_key.split('.', 1)
         conn = sqlite3.connect('/app/data/api_keys.db')
         cursor = conn.cursor()
-        hashed_entry_key = hashlib.sha256(entry_key.encode()).hexdigest()
-        cursor.execute("DELETE FROM api_keys WHERE api_key = ?", (hashed_entry_key,))
+        cursor.execute("DELETE FROM api_keys WHERE uid = ?", (uid_part,))
         if cursor.rowcount == 0:
             raise HTTPException(status_code=404, detail="API Key not found")
         else:
             conn.commit()
             conn.close()
             del entry_key
-            del hashed_entry_key
+            del key_part
+            del uid_part
             return {
                 "status": "200",
                 "message": "API Key deleted successfully"
             }
     except Exception as e:
+        print(f"Error deleting API key: {str(e)}", flush=True)
         raise HTTPException(status_code=500, detail="Error occurred while deleting API key")

21d57e93e70609d26b13757b2477a4da9356639d
CakieJoy <cakiejoy@utkan.net>
2026-07-20 02:10:16 +0300

    fix: if setting is not in the config gives the default value

--- app_config.py
+++ app_config.py
@@ -17,14 +17,16 @@ def reload_config():
     with open("/app/data/config.yaml", "r") as config_file:
         data = yaml.safe_load(config_file)
 
-    UPLOAD_FOLDER = data["settings"]["UPLOAD_FOLDER"]
-    DOMAIN = data["settings"]["DOMAIN"]
-    RAW_API_KEY = data["settings"]["API_KEY"]
-    IMAGE_URL_PREFIX = data["settings"]["URL_PREFIX"]
-    SUPPORTED_EXTENSIONS = data["supported_extensions"]
-    DISABLE_DOCS = data["settings"].get("DISABLE_DOCS")
-    BEHIND_PROXY = data["settings"].get("BEHIND_PROXY") 
-    
+    settings = data.get("settings", {})
+
+    UPLOAD_FOLDER = settings.get("UPLOAD_FOLDER", "images")
+    DOMAIN = settings.get("DOMAIN", "localhost:8000")
+    RAW_API_KEY = settings.get("API_KEY", "my_very_very_secret_api_key")
+    IMAGE_URL_PREFIX = settings.get("URL_PREFIX", "/images")
+    SUPPORTED_EXTENSIONS = data.get("supported_extensions", [".jpg", ".jpeg", ".png", ".gif"])
+    DISABLE_DOCS = settings.get("DISABLE_DOCS", True)
+    BEHIND_PROXY = settings.get("BEHIND_PROXY", True)
+
     return {"status": "200", "message": "Configuration reloaded successfully"}
 
 reload_config()

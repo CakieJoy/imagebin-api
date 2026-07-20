a93c871eaacf81c26cf6b4a9f7b925811181342a
CakieJoy <cakiejoy@utkan.net>
2026-07-20 23:24:45 +0300

    test: refactored upload_image test work pls

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -1,3 +1,4 @@
+from Tests.upload_test import test_upload_image_success, test_upload_image_with_invalid_api_key, test_upload_image_without_api_key, test_upload_image_without_image
 from fastapi.testclient import TestClient
 import pytest
 from main import app
@@ -43,11 +44,13 @@ def test_get_images_authv2():
     response = client.get("/api/v2/get-images", headers=header)
     assert response.status_code == 200
 
-def test_upload_image_authv2():
-    header = {"x-api-key": "1.very_secret_key_100_real"}
-    files = {'image': ('test_image.jpg', b"Example data", 'image/jpeg')}
-    response = client.post("/api/v2/upload", headers=header, files=files)
-    assert response.status_code == 200
+test_upload_image_success()
+
+test_upload_image_without_image()
+
+test_upload_image_without_api_key()
+
+test_upload_image_with_invalid_api_key()
 
 def test_delete_image(upload_image_id):
     header = {"x-api-key": "1.very_secret_key_100_real"}
---
+++ Tests/upload_test.py
@@ -0,0 +1,38 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+
+def test_upload_image_success():
+    header = {"x-api-key": "1.very_secret_key_100_real"}
+    files = {'image': ('test_image.jpg', b"Example data", 'image/jpeg')}
+
+    response = client.post("/api/v2/upload", headers=header, files=files)
+
+    assert response.status_code == 200
+
+def test_upload_image_without_image():
+    header = {"x-api-key": "1.very_secret_key_100_real"}
+    files = {}
+
+    response = client.post("/api/v2/upload", headers=header, files=files)
+
+    assert response.status_code == 422
+
+def test_upload_image_without_api_key():
+    header = {}
+    files = {'image': ('test_image.jpg', b"Example data", 'image/jpeg')}
+
+    response = client.post("/api/v2/upload", headers=header, files=files)
+
+    assert response.status_code == 401
+
+def test_upload_image_with_invalid_api_key():
+    header = {"x-api-key": "im_not_a_api_key_iam_a_teapot"}
+    files = {'image': ('test_image.jpg', b"Example data", 'image/jpeg')}
+
+    response = client.post("/api/v2/upload", headers=header, files=files)
+
+    assert response.status_code == 403
+

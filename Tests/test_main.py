ef00a8d94f1ad2df45ae4014a3bd554cf0621b97
CakieJoy <cakiejoy@utkan.net>
2026-07-20 23:51:54 +0300

    fix: reset limiter stroage on every test step

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -2,16 +2,26 @@ from Tests.upload_test import test_upload_image_success, test_upload_image_with_
 from fastapi.testclient import TestClient
 import pytest
 from main import app
+from main import limiter
 
 client = TestClient(app)
 
-@pytest.fixture
+
+
+
+@pytest.fixture(autouse=True)
+def clear_rate_limit():
+    limiter._storage.reset()
+    yield
+
+
 def upload_image_id():
     files = {'image': ('test_image.jpg', b"Example data", 'image/jpeg')}
     header = {"x-api-key": "my_very_very_secret_api_key"}
     response = client.post("/v1/upload", headers=header, files=files)
     img_id = response.json()["image_id"]
     yield img_id
+
 
 # * AuthV2 Tests
 
@@ -28,7 +38,7 @@ def test_get_images_authv2():
     header = {"x-api-key": "1.very_secret_key_100_real"}
     response = client.get("/api/v2/get-images", headers=header)
     assert response.status_code == 200
-    
+
 def test_delete_image(upload_image_id):
     header = {"x-api-key": "1.very_secret_key_100_real"}
     response = client.delete(f"/api/v2/delete/?image_id={upload_image_id}", headers=header)

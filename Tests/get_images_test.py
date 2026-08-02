c1c2d66d5381c706f5064459b6b28c9fcad87593
CakieJoy <cakiejoy@utkan.net>
2026-08-03 02:41:17 +0300

    test: Fixed wrong status codes on invalid key

--- Tests/get_images_test.py
+++ Tests/get_images_test.py
@@ -16,4 +16,4 @@ def test_get_images_without_api_key():
 def test_get_images_with_invalid_api_key():
     header = {"x-api-key": "im_not_a_api_key_iam_a_teapot"}
     response = client.get("/api/v2/get-images", headers=header)
-    assert response.status_code == 401
+    assert response.status_code == 400

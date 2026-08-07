04b555b825173d24a08ccc2b5b4c69a03b471dd4
CakieJoy <cakiejoy@utkan.net>
2026-08-08 02:56:43 +0300

    file path updated in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "[INFO] Checking config file existing"
 if [ ! -f "/app/data/config.yaml" ]; then
   echo "[WARN] Config file not found, copying default config..."
-  cp /app/config.template.yaml /app/data/config.yaml
+  cp /app/templates/config.template.yaml /app/data/config.yaml
 else
   echo "[INFO] Config file found, skipping copy."
 fi
@@ -13,7 +13,7 @@ echo "[INFO] Checking 404.html file existing"
 if [ ! -f "/app/data/templates/404.html" ]; then
   echo "[WARN] 404.html file not found, copying default..."
   mkdir -p /app/data/templates
-  cp /app/404.template.html /app/data/templates/404.html
+  cp /app/templates/404.template.html /app/data/templates/404.html
 else
   echo "[INFO] 404.html file found, skipping copy."
 fi

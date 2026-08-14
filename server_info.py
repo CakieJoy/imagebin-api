a4a27d9aa5b30fcb819eecaf83f6a662a714ccd6
CakieJoy <cakiejoy@utkan.net>
2026-08-14 23:46:36 +0300

    Add environment to info endpoint

--- server_info.py
+++ server_info.py
@@ -12,6 +12,7 @@ def server_info():
         "name": config.INSTANCE_NAME,
         "description": config.INSTANCE_DESCRIPTION,
         "version": get_app_version(),
+        "environment": config.ENVIRONMENT,
         "email": config.INSTANCE_EMAIL,
         "domain": config.DOMAIN,
         "url_prefix": config.IMAGE_URL_PREFIX,

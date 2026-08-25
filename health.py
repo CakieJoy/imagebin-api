60f5203715f334983fcad36666b2fc64a891bfb1
CakieJoy <cakiejoy@utkan.net>
2026-08-26 01:45:16 +0300

    start_time and response_time shows only ms (uses unix timestamp)

--- health.py
+++ health.py
@@ -3,8 +3,8 @@ from uptime import get_uptime
 
 
 def health_check():
-    response_time = time.time()
+    response_time = int(time.time() * 1000)  # * Store response time in ms
     return {
         "timestamp": response_time,
-        "uptimeSeconds": get_uptime()
+        "uptimeSeconds": get_uptime() // 1000
     }
--- uptime.py
+++ uptime.py
@@ -2,10 +2,10 @@ import time
 
 def create_start_time():
     global start_time
-    start_time = time.time()
+    start_time = int(time.time() * 1000)  # * Store start time in ms
 
 def get_start_time():
     return start_time
 
 def get_uptime():
-    return int(time.time() - start_time)
+    return int(time.time() * 1000) - start_time

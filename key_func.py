f4207e3ef58188ff6d56b15f3feba8e6b55fcd0b
CakieJoy <cakiejoy@utkan.net>
2026-08-26 20:39:53 +0300

    feat: add whitelist feature to ip check

--- key_func.py
+++ key_func.py
@@ -17,8 +17,14 @@ def key_func(request: Request):
         if config.BEHIND_PROXY:
             cf_connecting_ip = request.headers.get("CF-Connecting-IP")
             if cf_connecting_ip:
-                return cf_connecting_ip
+                if cf_connecting_ip in config.RATE_LIMIT_IP_WL:
+                    return None
+                else:
+                    return cf_connecting_ip
             x_forwarded_for = request.headers.get("X-Forwarded-For")
             if x_forwarded_for:
-                return x_forwarded_for.split(",")[-2].strip()
+                if x_forwarded_for.split(",")[-2].strip() in config.RATE_LIMIT_IP_WL:
+                    return None
+                else:
+                    return x_forwarded_for.split(",")[-2].strip()
     return get_remote_address(request)
